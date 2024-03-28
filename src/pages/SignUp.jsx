2338d0b461e445e88d72cb58936fe31168609a43
theprathyaksh <iamprathyaksh@gmail.com>
2024-03-28 14:57:13 +0530

    first commit

---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Signup as SignupComponent } from '../components'
+function SignUp() {
+  return (
+    <div className='py-8'>
+        <SignupComponent/>
+    SignUp</div>
+  )
+}
+
+export default SignUp
